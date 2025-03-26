c45e7e197b8c02e102fa94c14783911ee7d8c4db
paudel-Suman <sumanpaudel227@gmail.com>
2025-03-26 08:21:16 +0000

    save

---
+++ 01_basics/datatype.js
@@ -0,0 +1,17 @@
+"use strict"; // treat all js code as newer version
+
+// alert(3+3) we are using node js
+
+console.log(3 
+    + 3)  // code readeability should be high
+
+console.log("suman")
+
+let name = "suman"
+let age = 21
+let isLoggedIn = false
+
+// number => 2 to pwr 52
+// bigint 
+// string =""
+// boolean
---
+++ 01_basics/variables.js
@@ -0,0 +1,15 @@
+const accountId = 5656
+let accemail = "sppp@gmail.com"
+var accpass = "06598"
+accstate = "ktm"
+
+
+console.table([accountId, accemail, accpass, accstate])
+
+
+
+accemail = "pp@gmail.com"
+accpass = "9898"
+accstate = "986"
+
+console.table([accountId, accemail, accpass, accstate])
