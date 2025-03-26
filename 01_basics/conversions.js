39c26ce427e1957e80a1570d2c42757507f387a7
paudel-Suman <sumanpaudel227@gmail.com>
2025-03-26 11:57:13 +0000

    dfcxf

--- 01_basics/conversions.js
+++ 01_basics/conversions.js
@@ -4,3 +4,24 @@ console.log(typeof score);
 
 let valueInNumber = Number (score)
 console.log(typeof valueInNumber);
+
+
+// "33" = 33
+// "33aaa" = NAN
+// true = 1, false = 0
+
+let isloggedin = 1
+let booleanisloggedin = Boolean (isloggedin)
+console.log(booleanisloggedin);
+
+
+// 1 = true, 0 = false
+// "" = false
+// "hitesh" = true
+
+
+let somenumber = 54
+let stringnumber = String (somenumber)
+console.log(stringnumber);
+console.log(typeof stringnumber);
+
