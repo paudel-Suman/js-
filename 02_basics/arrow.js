80181f727cfc6df1dea31e71782f1b96de28b944
paudel-Suman <sumanpaudel227@gmail.com>
2025-04-13 11:45:50 +0000

    main

---
+++ 02_basics/arrow.js
@@ -0,0 +1,12 @@
+const user = {
+    usern: "suman",
+    price: 25,
+
+    welcomeMessage: function() {
+        console.log('{this.usern}, welcome to website');
+        
+    }
+
+}
+
+user.welcomeMessage()
--- 02_basics/objects.js
+++ 02_basics/objects.js
@@ -22,9 +22,10 @@ const regularUser = {
 // console.log(regularUser.fullname.username.firstname);
 
 const obj1 = {1: "a", 2: "b"}
-const obj2 = {3: "c", 4: "d"}
+const obj2 = {3: "a", 4: "b"}
+const obj4 = {5: "a", 6: "b"}
+// const obj3 = {obj1, obj2}
 
-const obj3 = {obj1, obj2}
-
+const obj3 = Object.assign({}, obj1, obj2, obj4)
 console.log(obj3);
 
